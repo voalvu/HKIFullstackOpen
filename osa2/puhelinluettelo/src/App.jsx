cd535b8b12b30f6cc1de1c9ed12a8886dbeb1b7f
voalvu <voalvu@gmail.com>
2024-09-20 06:32:37 +0300

    2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({setFil}) => {
     const handleFilterInput = (e) => {
@@ -48,13 +49,32 @@ const AddPersonForm = ({persons, setPersons}) =>{
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-      ])
+    const [notes, setNotes] = useState([])
+    const [persons, setPersons] = useState([])
   const [fil, setFil] = useState('')
+
+  
+  useEffect(() => {
+    console.log('notes effect')    
+    axios      
+      .get('http://localhost:3001/notes')      
+      .then(response => {        
+        console.log('promise fulfilled')        
+        setNotes(response.data)      
+      })  
+    }, [])  
+    console.log('render', notes.length, 'notes')
+
+    useEffect(() => {
+      console.log('persons effect')    
+      axios      
+        .get('http://localhost:3001/persons')      
+        .then(response => {        
+          console.log('promise fulfilled')        
+          setPersons(response.data)      
+        })  
+      }, [])  
+      console.log('render', persons.length, 'persons')
 
   return (
     <>
