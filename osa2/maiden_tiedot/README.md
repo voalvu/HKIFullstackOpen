c1ef948fc69bcf8a962564d027e9b5958c131658
voalvu <voalvu@gmail.com>
2024-09-28 15:16:39 +0300

    2.20

---
+++ osa2/maiden_tiedot/README.md
@@ -0,0 +1,4 @@
+Weather icon mapping source: "https://gist.github.com/stellasphere/"
+
+direct link: 
+https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c
--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -1,25 +1,58 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import weather_descriptions from './descriptions.json'
+
 
 
 const CountryInfo = ({country}) =>{
+  const [weatherObj, setWeatherObj] = useState({})
+  const api_key = import.meta.env.VITE_WEATHER_API
+
+  useEffect(()=>{
+    if (country){
+      //console.log(country)
+      axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${country.capitalInfo.latlng[0]}&longitude=${country.capitalInfo.latlng[1]}&current=temperature_2m,wind_speed_10m,weather_code&wind_speed_unit=ms`)
+        .then((res)=>{
+          //console.log(res.data);
+          setWeatherObj(res.data);
+      })}
+  }, [country])
+
   if (country == null)  return (<div></div>)
-  else return (
-  <div>
-    <h1>{country.name.common}</h1>
-    <br></br>
-    capital {country.capital}
-    <br></br> 
-    area {country.area}
-    <h3>languages</h3>
-    <ul>
-      <li>
-        {Object.values(country.languages).map(lang=><li>{lang}</li>)}
-      </li>
-    </ul>
-    {<img src={country.flags.png}></img>}
+    //console.log(weatherObj)
+  return (
+  
+    <div>
+      <h1>{country.name.common}</h1>
+      <br></br>
+      capital {country.capital}
+      <br></br> 
+      area {country.area}
+      
+      <h3>languages</h3>
+      <ul>
+        <li>
+          {Object.values(country.languages).map(lang=><li key={lang}>{lang}</li>)}
+        </li>
+      </ul>
+
+      {<img src={country.flags.png}></img>}
+      
+      <h3>Weather in {country.capital}</h3>
+      {/* The questionmarks help to render the component when the data hasn't been received yet from API */}
+      {`${weatherObj.current?.temperature_2m} ${weatherObj.current_units?.temperature_2m}`}
+      <br></br>
+      
+      {/* Weather codes mapped to Open weather map icons. Source in README.md */}
+      {/* Perform check that both used variables have been set before rendering img */}
+      {weatherObj.current?.weather_code && weather_descriptions[weatherObj.current.weather_code]
+        ? <img src={weather_descriptions[weatherObj.current.weather_code].day.image}></img>
+        : null}
+      
+      <br></br>
+      {`${weatherObj.current?.wind_speed_10m} ${weatherObj.current_units?.wind_speed_10m}`}
     </div>
-  ) 
+  )
 }
 
 const App = () => {
@@ -30,22 +63,20 @@ const [foundCountries,setFoundCountries] = useState([])
 const [singleCountry, setSingleCountry] = useState(null)
 
 
-/* axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
-  .then((res)=>console.log(res.data)) */
-//console.log(inputText)
-
 const handleInput =(e) =>
 {
   setInput(e.target.value)
-
 }
 
+// get all countries on first page load
 
-// get all countries
+// ALSO
+// Mock API key printed in console to demonstrate my understanding of Vite .env variables.
+
 useEffect(() => {
-  console.log("effect")
+  console.log("first page load effect")
     axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then((res)=>setCountries(res.data))
+      .then((res)=>{setCountries(res.data); console.info(`MOCK API KEY USE, AS WE DIDN'T USE ONE FOR WEATHER DATA\n ${import.meta.env.VITE_SOME_KEY}`)})
 },[])
 
 // update foundCountries list whenever inputText changes. 
@@ -75,7 +106,7 @@ useEffect(() => {
       find countries <input type="text" onInput={handleInput}></input>
       <div></div>
       <p>Found:</p> {foundCountries.map(c => <p>{c}</p>)}
-      <CountryInfo country={singleCountry}></CountryInfo>
+      <CountryInfo country={singleCountry} weather_descriptions={weather_descriptions}></CountryInfo>
     </div>
     </>
   )
