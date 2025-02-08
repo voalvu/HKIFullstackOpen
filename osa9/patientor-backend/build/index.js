8e2da9631208704fe02c3e30f4660f13fc603d8f
voalvu <voalvu@gmail.com>
2025-02-08 17:53:04 +0100

    9.9 pong from frontend working

--- osa9/patientor-backend/build/index.js
+++ osa9/patientor-backend/build/index.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/ping', (_req, res) => {
     res.send("pong");
 });
 const PORT = 3003;
 app.listen(PORT, () => {
-    console.log(`server running ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (_req,res)=>{
+app.get('/ping', (_req, res) => {
     res.send("pong");
+});
+
+app.get('/patients',(_req,res)=>{
+    res.send("temp");
 });
 
 const PORT = 3003;
 
-app.listen(PORT,()=>{
-    console.log(`server running ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
 });
