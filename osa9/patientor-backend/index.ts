01c881873290c251cad02916d05ad6cdf2e69c6c
voalvu <voalvu@gmail.com>
2025-02-08 17:41:34 +0100

    9.8 init ts backend pong, build and run

---
+++ osa9/patientor-backend/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (_req,res)=>{
+    res.send("pong");
+});
+
+const PORT = 3003;
+
+app.listen(PORT,()=>{
+    console.log(`server running ${PORT}`);
+});
